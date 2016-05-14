b2aa63512c3d204028dc23248316b979e230625e
MedAziz1 <mdacheche@gmail.com>
2016-05-14 17:28:20 -0400

    Added AuthenticayionController

--- src/main/java/io/tobima/backend/api/authentication/AuthenticationController.java
+++ src/main/java/io/tobima/backend/api/authentication/AuthenticationController.java
@@ -1,5 +1,15 @@
 package io.tobima.backend.api.authentication;
 
+import com.google.api.server.spi.config.Api;
+import com.google.api.server.spi.config.ApiNamespace;
+
+@Api(name = "myApi",
+version = "v1",
+namespace = @ApiNamespace(ownerDomain = "io.tobima.backend",
+    ownerName = "io.tobima.backend",
+    packagePath = ""))
+
+
 public class AuthenticationController {
 
 }
